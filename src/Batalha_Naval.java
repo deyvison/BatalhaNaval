aea94434f8b245c690dbdbdab8f075905c79f0a1
Deyvison Oliveira <deyvison.melo@dce.ufpb.br>
2015-04-24 12:59:29 -0300

    version 1.0

--- src/Batalha_Naval.java
+++ src/Batalha_Naval.java
@@ -5,70 +5,63 @@ public class Batalha_Naval {
 
 	public static void main(String[] args) throws Exception{
 		
-	try{
+	
 		
 	
 		Tabuleiro t1, t2;
 	
-			t1 = new Tabuleiro(15, 15); // linha,coluna
-			t2 = new Tabuleiro(15, 15);
+		t1 = new Tabuleiro(15, 15); // linha,coluna
+		t2 = new Tabuleiro(15, 15);
 	
-			t1.adicionarEmbarcacaoArquivo(args[0]);
-			t2.adicionarEmbarcacaoArquivo(args[1]);
+		t1.adicionarEmbarcacaoArquivo(args[0]);
+		t2.adicionarEmbarcacaoArquivo(args[1]);
 			
-			if(t1.isCarregado() && t2.isCarregado()) 
-				System.out.println("TABULEIROS CARREGADOS COM SUCESSO\n");
+		if(t1.isCarregado() && t2.isCarregado()) 
+			System.out.println("TABULEIROS CARREGADOS COM SUCESSO\n");
 			
 			
 		
-			InputStreamReader ir = new InputStreamReader(System.in);
-			BufferedReader leitor = new BufferedReader(ir);
+		InputStreamReader ir = new InputStreamReader(System.in);
+		BufferedReader leitor = new BufferedReader(ir);
 			
-			String dados[];
-			String jogadorVez = "J1";
+		String dados[];
+		String jogadorVez = "J1";
 			
-			while(true){
-				if(t1.getEmbarcacoesAfundadas() >= 5){
-					System.out.println("J2 GANHOU!");
-					break;
-				}else if(t2.getEmbarcacoesAfundadas() >= 5){
-					System.out.println("J1 GANHOU!");
-					break;
-				}else{
-					// jogador jogar
-					if(jogadorVez.equals("J1")){
-						while(true){
-							System.out.print(jogadorVez+"> ");
-							dados = leitor.readLine().split(" ");
-							String retorno = t2.atirar(dados[0], dados[1]);
-							System.out.println(retorno);
+		while(true){
+			
+			if(t1.getEmbarcacoesAfundadas() >= 5){
+				System.out.println("J2 GANHOU!");
+				break;
+			}else if(t2.getEmbarcacoesAfundadas() >= 5){
+				System.out.println("J1 GANHOU!");
+				break;
+			}else{
+				// jogador jogar
+				if(jogadorVez.equals("J1")){
+					while(true){
+						System.out.print(jogadorVez+"> ");
+						dados = leitor.readLine().split(" ");
+						String retorno = t2.atirar(dados[0], dados[1]);
+						System.out.println(retorno);
 							
-							if(retorno.equals("AGUA"))
-								jogadorVez = "J2";
-								break;
-						}
+						if(retorno.equals("AGUA"))
+							jogadorVez = "J2";
+							break;
+					}
 						
-					}else{
-						while(true){
-							System.out.print(jogadorVez+"> ");
-							dados = leitor.readLine().split(" ");
-							String retorno = t1.atirar(dados[0], dados[1]);
-							System.out.println(retorno);
-							
-							if(retorno.equals("AGUA"))
-								jogadorVez = "J1";
-								break;
-						}
+				}else{
+					while(true){
+						System.out.print(jogadorVez+"> ");
+						dados = leitor.readLine().split(" ");
+						String retorno = t1.atirar(dados[0], dados[1]);
+						System.out.println(retorno);
+						
+						if(retorno.equals("AGUA"))
+							jogadorVez = "J1";
+							break;
 					}
 				}
-					
-				
-				
 			}
-		}catch(Exception e){
-			System.out.println("JOGADA INVALIDA");
 		}
-			
 	}
-
 }
